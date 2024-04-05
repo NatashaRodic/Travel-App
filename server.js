18a55bd17d93d34f1d2d0d1c80652b7867158411
Natasha Rodic <natashawolt@gmail.com>
2024-04-05 16:13:44 -0400

    Define routes for authentication

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use(methodOverride('_method'))
 
 app.use('/', indexRouter);
