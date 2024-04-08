52a67913a630b73ff4eba1c68a5d9e38ce1bc913
Natasha Rodic <natashawolt@gmail.com>
2024-04-08 13:19:39 -0400

    Code the Controller Action

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,11 +2,16 @@ const User = require('../models/user');
 const Trip = require('../models/trip')
 
 module.exports = {
-    index
+    index,
+    new: newTrip
 }
 
 async function index(req, res) {
     const trips = await Trip.find({})
     console.log(trips)
     res.render('trips/index', { title: 'All Trips', trips });
+}
+
+function newTrip(req, res) {
+    res.render('trips/new', { title: 'Create a new Trip', errorMsg: '' })
 }
